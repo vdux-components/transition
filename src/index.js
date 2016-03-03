2d1c145969aa3dde82fc16cc0e8892a2745a9b09
Andrew Shaffer <andrew@weo.io>
2016-03-03 13:22:59 -0800

    refactor; make it work more

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@
 
 import handleActions from '@f/handle-actions'
 import createAction from '@f/create-action'
-import difference from '@f/difference'
-import filter from '@f/filter-array'
 import element from 'vdux/element'
 import splice from '@f/splice'
 import map from '@f/map-array'
+import filter from '@f/filter'
 import index from '@f/index'
 import omit from '@f/omit'
+import has from '@f/has'
 
 /**
  * Transition component
@@ -21,19 +21,14 @@ function initialState ({children, local}) {
 
   return {
     children,
-    childStates: index(getKey, child => initialChildState(child, local), children)
+    childStates: index(getKey, () => ({entering: true}), children)
   }
 }
 
 function onUpdate (prev, next) {
   if (prev.children !== next.children) {
     assertKeyed(next.children)
-
-    const {local} = next
-    return [
-      map((child, idx) => local(willEnter)({child, idx, local}), difference(next.children, prev.children, getKey)),
-      map(child => local(willLeave)(getKey(child)), difference(prev.children, next.children, getKey))
-    ]
+    return next.local(updateChildren)(next.children)
   }
 }
 
@@ -46,7 +41,7 @@ function render ({state, local}) {
           props: {
             ...child.props,
             transition: {
-              ...state.childStates[getKey(child)],
+              ...(state.childStates[getKey(child)] || {}),
               didEnter: local(didEnter, getKey(child)),
               didLeave: local(didLeave, getKey(child))
             }
@@ -61,48 +56,33 @@ function render ({state, local}) {
  * Actions
  */
 
-const willEnter = createAction('<Transition/>: component will enter')
 const didEnter = createAction('<Transition/>: component did enter')
-
-const willLeave = createAction('<Transition/>: component will leave')
 const didLeave = createAction('<Transition/>: component did leave')
+const updateChildren = createAction('<Transition/>: update children')
 
 /**
  * Reducer
  */
 
 const reducer = handleActions({
-  [willEnter]: (state, {child, idx, local}) => ({
-    ...state,
-    children: insertAt(child, idx, state.children),
-    childStates: {
-      ...state.childStates,
-      [getKey(child)]: state.childStates[getKey(child)] || initialChildState(child, local)
-    }
-  }),
-  [didEnter]: (state, key) => ({
-    ...state,
-    childStates: {
-      ...state.childStates,
-      [key]: {
-        ...state.childStates[key],
-        entering: false
+  [updateChildren]: (state, nextChildren) => mergeChildren(state.children, nextChildren),
+  [didEnter]: (state, key) => {
+    const childState = state.childStates[key]
+
+    if (childState && childState.entering) {
+      return {
+        ...state,
+        childStates: childState.leaving
+          ? {...childStates, [key]: {...childState, entering: false}}
+          : omit(state.childStates, key)
       }
     }
-  }),
-  [willLeave]: (state, key) => ({
-    ...state,
-    childStates: {
-      ...state.childStates,
-      [key]: {
-        ...state.childStates[key],
-        leaving: true
-      }
-    }
-  }),
+
+    return state
+  },
   [didLeave]: (state, key) => ({
     ...state,
-    children: withoutKey(key, state.children),
+    children: filter(item => getKey(item) !== key, state.children),
     childStates: omit(state.childStates, key)
   })
 })
@@ -111,10 +91,61 @@ const reducer = handleActions({
  * Helpers
  */
 
-function initialChildState () {
+function mergeChildren (prev, next) {
+  const prevMap = index(getKey, prev)
+  const nextMap = index(getKey, next)
+  const pendingMap = {}
+  const childStates = {}
+  let pendingKeys = []
+  const children = []
+
+  for (let i = 0; i < prev.length; ++i) {
+    const child = prev[i]
+    const key = getKey(child)
+
+    if (!has(key, nextMap)) {
+      childStates[key] = {
+        leaving: true
+      }
+      pendingKeys.push(key)
+    } else if (pendingKeys.length) {
+      pendingMap[key] = pendingKeys
+      pendingKeys = []
+    }
+  }
+
+  for (let i = 0; i < next.length; ++i) {
+    const child = next[i]
+    const key = getKey(child)
+
+    if (!has(key, prevMap)) {
+      childStates[key] = {
+        entering: true
+      }
+    }
+
+    if (has(key, pendingMap)) {
+      const pending = pendingMap[key]
+
+      for (let j = 0; j < pending.length; ++j) {
+        children.push(getChild(pending[j]))
+      }
+    }
+
+    children.push(child)
+  }
+
+  for (let i = 0; i < pendingKeys.length; ++i) {
+    children.push(getChild(pendingKeys[i]))
+  }
+
   return {
-    entering: true,
-    leaving: false
+    childStates,
+    children
+  }
+
+  function getChild (key) {
+    return has(key, nextMap) ? nextMap[key] : prevMap[key]
   }
 }
 
@@ -122,12 +153,12 @@ function getKey (vnode) {
   return vnode.key
 }
 
-function withoutKey (key, list) {
-  return filter(item => getKey(item) !== key, list)
-}
-
 function insertAt (item, idx, list) {
-  return splice(list, idx, 0, item)
+  const key = getKey(item)
+
+  return list.some(child => getKey(child) === key)
+    ? list
+    : splice(list, idx, 0, item)
 }
 
 function assertKeyed (children) {
